7cbcc989ac2b63b30b839094e26be8c6544d846a
k9193 <1004278878@qq.com>
2024-10-25 10:37:03 +0800

    10_25

--- lcof-python/LeetCode/9_BackTrack_Greedy/218_121_买卖股票的最佳时机_9_27.py
+++ lcof-python/LeetCode/9_BackTrack_Greedy/218_121_买卖股票的最佳时机_9_27.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # 这里的 dp 是一个列表，用来存储目前为止的最低买入价格
+        # 下面是贪心算法 dp是二维数组，dp[i][0]表示第i天持有，dp[i][1]表示第i天未持有
         dp = [prices[0]]
         val = 0
         for i in range(1, len(prices)):
