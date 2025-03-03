c12c188f98bceda54fba5f12d364bfac9ce75dad
k9193 <1004278878@qq.com>
2025-03-03 16:43:05 +0800

    3/3

--- lcof-cpp/offer/2_Tree/14_二叉搜索树的第k个结点.cpp
+++ lcof-cpp/offer/2_Tree/14_二叉搜索树的第k个结点.cpp
@@ -1,31 +1,49 @@
 /**
- * struct ListNode {
+ * struct TreeNode {
  *	int val;
- *	struct ListNode *next;
- *	ListNode(int x) : val(x), next(nullptr) {}
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ *	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  * };
  */
+#include <memory>
 class Solution
 {
+    int count{0};
+    int result{-1};
+
 public:
     /**
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
      *
      *
-     * @param head ListNode类
-     * @return ListNode类
+     * @param proot TreeNode类
+     * @param k int整型
+     * @return int整型
      */
-    ListNode *ReverseList(ListNode *head)
+
+    void inOrder(TreeNode *proot, int k)
     {
-        ListNode *pre = nullptr;
-        auto cur = head;
-        while (cur != nullptr)
+        if (!proot)
+            return;
+        inOrder(proot->left, k);
+        ++count;
+        if (count == k)
         {
-            auto temp = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = temp;
+            result = proot->val;
+            return;
         }
-        return pre;
+        inOrder(proot->right, k);
+        return;
+    }
+
+    int KthNode(TreeNode *proot, int k)
+    {
+        if (proot == nullptr)
+            return -1;
+        count = 0;
+        result = -1;
+        inOrder(proot, k);
+        return result;
     }
 };
