762c442cdf2b8530a53d4cb3a1c799d0245a6f7f
k9193 <1004278878@qq.com>
2025-10-09 20:15:37 +0800

    [笔试整理]

--- lcof-cpp/LeetCode/test.cpp
+++ lcof-cpp/LeetCode/test.cpp
@@ -5,37 +5,22 @@
 #include <utility>
 #include <vector>
 using namespace std;
-
+struct alignas(4) Test
+{
+    char a;   // 1 字节
+    double b; // 8 字节
+    char c;   // 1 字节
+};
+struct alignas(4) Info2
+{
+    uint8_t a;
+    uint16_t b;
+    uint8_t c;
+};
 int main()
 {
-    vector<int> v;
-    int n;
-    cin >> n;
-    while (n--)
-    {
-        int m;
-        cin >> m;
-        v.emplace_back(m);
-    }
-    int s = std::accumulate(v.begin(), v.end(), 0);
-    int maxDiff = -2147483648;
-    int lSum = v[0];
-    int rSum = s - lSum;
-    pair<int, int> p(maxDiff, 0);
-    for (int i = 1; i < v.size() - 1; ++i)
-    {
-        long diff = rSum - lSum;
-        if (diff > maxDiff)
-        {
-            p.first = diff;
-            p.second = i;
-        }
-        rSum -= v[i];
-        lSum += v[i];
-    }
-    int l = std::accumulate(v.begin(), v.begin() + p.second, 0);
-    int r = std::accumulate(v.begin() + p.second, v.end(), 0);
-
-    return l * r;
+    cout << sizeof(Test) << endl;
+    cout << sizeof(Info2) << endl;
+    return 0;
 }
 // 64 位输出请用 printf("%lld")
